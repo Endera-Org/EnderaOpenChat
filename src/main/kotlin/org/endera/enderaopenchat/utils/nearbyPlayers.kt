deaeae23efffd65191af2757e67daaf5e45f3fa2
Sculmix <vyacheslavbisquit@gmail.com>
2025-10-25 17:11:18 +0300

    cleanup

--- src/main/kotlin/org/endera/enderaopenchat/utils/nearbyPlayers.kt
+++ src/main/kotlin/org/endera/enderaopenchat/utils/nearbyPlayers.kt
@@ -7,7 +7,7 @@ import org.endera.enderaopenchat.integrations.CMIVanishHook
 
 fun nearbyPlayers(centerPlayer: Player, otherPlayers: Collection<Player>, range: Int): List<Player> {
     val playerLocation = centerPlayer.location
-    val rangeSquared = range.toDouble() * range.toDouble()
+    val rangeSquared = range * range
     val isCmi = EnderaOpenChat.integrations[Integrations.CMI] != null
 
     return otherPlayers.filter { other ->
@@ -16,7 +16,7 @@ fun nearbyPlayers(centerPlayer: Player, otherPlayers: Collection<Player>, range:
         } else if (isCmi) {
             CMIVanishHook.isPlayerVanished(other)
         } else {
-            other.hasMetadata("vanished") && other.getMetadata("vanished").any { it.asBoolean() }
+            other.getMetadata("vanished").any { it.asBoolean() }
         }
 
         if (isVanished) {
